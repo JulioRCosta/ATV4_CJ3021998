cb34157c846ae0bbf3454da64828e4ef2a940175
CJ3021998 <CJ3021998@LAB01-PC16>
2024-03-20 08:35:49 -0300

    Adicionar arquivos de projeto.

---
+++ ATV4_CJ3021998/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+int n1, n2, oper, res;
+
+Console.WriteLine("Bem vindo a calculadora 2.0");
+Console.WriteLine("1 - Adição 2- Subtração 3- Multiplicação 4- Divisão");
+oper = int.Parse (Console.ReadLine());
+switch (oper){
+   case 1:
+        Console.WriteLine("Dê o primeiro valor");
+        n1 = int.Parse (Console.ReadLine());
+        Console.WriteLine("Dê o segundo valor ");
+        n2 = int.Parse (Console.ReadLine());
+        res = (n1 + n2);
+        Console.WriteLine("o reslutado da operação solicitada é {0}", res);
+        break;
+
+        case 2:
+        Console.WriteLine("Dê o primeiro valor");
+        n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Dê o segundo valor ");
+        n2 = int.Parse(Console.ReadLine());
+        res = (n1 - n2);
+        Console.WriteLine("o reslutado da operação solicitada é {0}", res);
+        break;
+
+        case 3:
+       
+        Console.WriteLine("Dê o primeiro valor");
+        n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Dê o segundo valor ");
+        n2 = int.Parse(Console.ReadLine());
+        res = (n1 * n2);
+        Console.WriteLine("o reslutado da operação solicitada é {0}", res);
+        break;
+
+        case 4:
+        Console.WriteLine("Dê o primeiro valor");
+        n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Dê o segundo valor ");
+        n2 = int.Parse(Console.ReadLine());
+        res = (n1 / n2);
+        Console.WriteLine("o reslutado da operação solicitada é {0}", res);
+        break;
+        
+        default:
+        Console.WriteLine("Opção invalida");
+        break;
+}
+*/
+
